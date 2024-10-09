a41789cf78e1426143f84711d4e7a0d89abc63cd
student <braden.schnaufer@atlasschool.com>
2024-10-09 20:54:38 +0000

    Vowel look at tolower

---
+++ Week1/Vowel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+
+System.Console.WriteLine("Please enter a single character: ");
+char letter = char.Parse(Console.ReadLine());
+letter = char.ToLower(letter);
+switch(letter)
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    System.Console.WriteLine("Vowel");
+    break;
+    case 'y':
+    System.Console.WriteLine("Maybe");
+    break;
+    default:
+    System.Console.WriteLine("Not a vowel.");
+    break;
+}
